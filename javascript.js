e46a3f4673d6b06e9179fb5bad1fcd909ff8abe6
reasonablep <140934359+reasonablep@users.noreply.github.com>
2023-09-08 18:49:20 -0700

    changed local file structure to make projects display alphabetically in finder.

--- javascript.js
+++ javascript.js
@@ -1,30 +1,29 @@
-function generateQuiz();
+let displayEl = $('#display');
+let startGameButton = $('#start-game-btn');
+let quizQuestions = $('#questions');
 
-let quizContainer = document.getElementById('quiz');
-let resultsContainer = document.getElementById('results');
-let submitButton = document.getElementById('submit');
 
-function renderQuestion(quizQuestions, quizContainer) {
+    function quizQuestions ()
+        let questions = [ {
 
-    let quizQuestions = [
-        {
-            Q1: "What is the opening tag for an array?", 
-            Answers: {
-                a. "[",
-                b. "(",
-                c. "/",
-                d. "<"
-            },
-            correctAnswer: 'a'
+            "Q1: What is the opening tag for an array?", 
+            const answers {
+                a: "[",
+                b: "(",
+                c: "/",
+                d: "<"
+            };
+            const correctAnswer{'a'}
+
         },
     
         {
             Q2: "What is the HTML tag for linking a JavaScript file?",
-            Answers: {
-                a. "link",
-                b. "style",
-                c. "src",
-                d. "script"
+            answers {
+                a: "link",
+                b: "style",
+                c: "src",
+                d: "script"
             },
     
             correctAnswer: 'd'
@@ -34,11 +33,11 @@ function renderQuestion(quizQuestions, quizContainer) {
         {
     
             Q3: "What is the proper way to call a JavaScript function?",
-            Answers: {
-                a. ';',
-                b. '[]',
-                c. '<>',
-                d. '()',
+            answers: {
+                a: ';',
+                b: '[]',
+                c: '<>',
+                d: '()',
     
             },
     
@@ -49,11 +48,11 @@ function renderQuestion(quizQuestions, quizContainer) {
             {
     
         Q4: "What is the symbol for a JQUERY in JavaScript?",
-            Answers: {
-            a. "+",
-            b. "&",
-            c. "$",
-            d. "#"
+            answers: {
+            a: "+",
+            b: "&",
+            c: "$",
+            d: "#"
     
             },
     
@@ -65,11 +64,11 @@ function renderQuestion(quizQuestions, quizContainer) {
     
     
             Q5: "What is the function of .length?",
-            Answers: {
-                a. "It returns the number of elements in an array or object",
-                b. "It styles the length of an element",
-                c. "It takes an argument, which increases the length of an array or obect by 1",
-                d. "It looks nice"
+            answers: {
+                a: "It returns the number of elements in an array or object",
+                b: "It styles the length of an element",
+                c: "It takes an argument, which increases the length of an array or obect by 1",
+                d: "It looks nice"
     
             },
     
@@ -80,11 +79,11 @@ function renderQuestion(quizQuestions, quizContainer) {
         { 
     
             Q6: "The arguments in a for loop are, ",
-            Answers: {
-                a. "i=;, i><;, i+-",
-                b. "(elementName)",
-                c. "Variables",
-                d. "i><; and i+"
+            answers: {
+                a: "i=;, i><;, i+-",
+                b: "(elementName)",
+                c: "Variables",
+                d:"i><; and i+"
     
             },
     
@@ -95,11 +94,11 @@ function renderQuestion(quizQuestions, quizContainer) {
         {
     
             Q7: "An example of a boolean is,"
-            Answers: {
-                a. "A string of numbers",
-                b. "Letters in quotation marks",
-                c. "An array of objects",
-                d. "A true or false value"
+            answers: {
+                a: "A string of numbers",
+                b: "Letters in quotation marks",
+                c: "An array of objects",
+                d:"A true or false value"
     
             },
     
@@ -110,11 +109,11 @@ function renderQuestion(quizQuestions, quizContainer) {
         {
     
             Q8: "What is an example of a truthy value?",
-            Answers: {
-                a. "0",
-                b. "Infinity",
-                c. "true == false",
-                d. "null"
+            answers: {
+                a: "0",
+                b: "Infinity",
+                c: "true == false",
+                d:"null"
     
             },
     
@@ -122,56 +121,15 @@ function renderQuestion(quizQuestions, quizContainer) {
     
         }
 
-    ];
+        startGameButton.on('click', '<div>', function (event){
 
-    quizContainer.innerHTML = output.join(''); {
-
-
-    for (let i=0; i < quizQuestions.length; i++) {
-
-        userAnswer = [];
-    
-        for (letter in quizQuestions[i].Answers) {
-
-            answers.push("card" + )
+            let startGameBtn = $('<div>');
         
-            );
-
-            output.push(".questions")
-
-            );
-        }; 
-
-
-    }
-
-    addEventListener('click', renderQuestion (quizQuestions, quizContainer));
-
-
-
-
-    
-
-function showResults(quizQuestions, quizContainer, resultsContainer) {
-
-    let resultsContainer = quizContainer.querySelectorAll('answers')
-
-   let userAnswer= ''; 
-   let correctCount = 0; 
-}
-
-for (let i=0; i<questions.length; i++) {
-
-    userAnswer= (answerContainers[i].querySelector('#answers')); {
-        if (userAnswer === questions[i].correctAnswer) {
-
-            numCorrect++; 
-            setItem (answerContainers);
+            startGameButton.addClass('button');
+        
+            startGameButton.text($(event.target).attr('data-question'));
+        
+            startGameButton.text(quizQuestions, "Let's start the game!");
+        
+            startGameBtn();
         }
-    }
-}
-
-
-submitButton.onclick = function(showResults);
-
-function generateQuiz(questions, quizContainer, resultsContainer, submitButton);
